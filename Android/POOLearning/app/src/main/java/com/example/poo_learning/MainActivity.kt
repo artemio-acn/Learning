eb4fbaf37e7225084c023274ef7351b2266147d5
Artemio Pánuco <artemio.panuco@accenture.com>
2024-11-25 00:40:04 -0600

    ANDROID-UPDATE: Not much working. Keeping on Advanced Kotlin Questions about lambda

--- Android/POOLearning/app/src/main/java/com/example/poo_learning/MainActivity.kt
+++ Android/POOLearning/app/src/main/java/com/example/poo_learning/MainActivity.kt
@@ -17,6 +17,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var pokemon: Pokemon
+
+    private fun String.noSpace(): String { //Method extension in a class (in this example, : String)
+        this.replace(" ", "")
+        return this
+    }
+
+    private fun IntArray.show() {
+        print("[")
+        for(i in this) print("$i")
+        println()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,22 +38,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        print("Sum: $func(1, 2))")
 
-        val person: Person = Person(name = "John Doe", passport = "AFF1233SDL0L")
-        println(person.name + " with passport number " + person.passport)
-
-        person.setNameAndPassport()
-        println(person.name + " with passport number " + person.passport)
-
-        val atleth: Athlete = Athlete(name = "Pelé", passport = "CCDA112ADSD", sport = "Soccer")
-        println(atleth.isAlive)
-
-        var pok: Pokemon = Pokemon(name = "Pikachu", attackPower = 500f, life = 100f)
-        pok.currentState()
-        pok.setAttackPower(200f)
-        pok.currentState()
-
+//
+//        val person: Person = Person(name = "John Doe", passport = "AFF1233SDL0L")
+//        println(person.name + " with passport number " + person.passport)
+//
+//        person.setNameAndPassport()
+//        println(person.name + " with passport number " + person.passport)
+//
+//        val atleth: Athlete = Athlete(name = "Pelé", passport = "CCDA112ADSD", sport = "Soccer")
+//        println(atleth.isAlive)
+//
+//        var pok: Pokemon = Pokemon(name = "Pikachu", attackPower = 500f, life = 100f)
+//        pok.currentState()
+//        pok.setAttackPower(200f)
+//        pok.currentState()
     }
+
+    fun executeHighOrderFunction(n1: Int, n2: Int, function: (Int, Int) -> Int) {
+        function(n1, n2)
+    }
+
+    fun increase(n1: Int, n2: Int): Int {
+        return n1 + n2
+    }
+
+    fun decrease(n1: Int, n2: Int): Int {
+        return n1 - n2
+    }
+
+    fun divide(n1: Int, n2: Int): Int {
+        return n1 / n2
+    }
+
+    fun multipy(n1: Int, n2: Int) = n1 * n2
+
+    var func = { x: Int, y: Int -> x+y }
+
 //    fun createPokemon(v: View) {
 //        val fieldName = findViewById<EditText>(R.id.textFieldName)
 //        val fieldAttachPower = findViewById<EditText>(R.id.textFieldAttachPower)
@@ -59,11 +93,13 @@ class MainActivity : AppCompatActivity() {
 //
 //    }
 
-    fun loadPokemonData(textViewPokemon: TextView, pokemon: Pokemon) {
-        var description: String = ""
-        description += pokemon.getName() + " ("
-        description += "Attack power: " + pokemon.getAttackPower()
-        description += " - Life: " + pokemon.getLife().toInt() + ")"
-        textViewPokemon.text = description
-    }
+//    fun loadPokemonData(textViewPokemon: TextView, pokemon: Pokemon) {
+//        var description: String = ""
+//        description += pokemon.getName() + " ("
+//        description += "Attack power: " + pokemon.getAttackPower()
+//        description += " - Life: " + pokemon.getLife().toInt() + ")"
+//        textViewPokemon.text = description
+//    }
+
+
 }
