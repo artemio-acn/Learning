c2c145304bd33f1f035993a96340e219dab6ed40
Artemio Pánuco <artemio.panuco@accenture.com>
2024-11-26 01:30:01 -0600

    FLUTTER - Using StatelessWidget to build UI

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,40 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    // const Center(
-    //   child: Text(
-    //   'Hello world',
-    //   textDirection: TextDirection.ltr,
-    // ),),
-    //
-    MaterialApp(
-      title: "Title",
-      home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard',),
-        body: Center(
-          child: Text.rich(
-            TextSpan(text: 'Text',
-            children: [
-              TextSpan(text: 'New span', 
-                  style: TextStyle(fontSize: 50.0, 
-                      fontWeight: FontWeight.bold)),
-              TextSpan(text: 'Other span',
-                  style: TextStyle(fontSize: 10)),
-            ])
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
+void main() => runApp(MyApp());
 
 // * Fat Arrows
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: Implement build
-    throw UnimplementedError()
+    return MaterialApp(
+      title: 'Temporary title',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard title'),
+          backgroundColor: Colors.pinkAccent,
+        ),
+        body: Center(
+            child: Text.rich(
+                TextSpan(text: 'Text',
+                    children: [
+                      TextSpan(text: 'New span',
+                          style: TextStyle(fontSize: 50.0,
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(text: 'Other span',
+                          style: TextStyle(fontSize: 10)),
+                    ])
+            ),
+        ),
+      ),
+    );
   }
 }
